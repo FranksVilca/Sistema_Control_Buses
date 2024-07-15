e728eb8dc9f0324fdc8d25e7ecaf05df01e84fb3
Willy Alexander Huisa Perez <whuisa@unsa.edu.pe>
2024-07-15 02:58:46 -0500

    Se inicio el dashboard login

--- proyecto-ips/src/App.js
+++ proyecto-ips/src/App.js
@@ -1,21 +1,12 @@
-import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PrimerComponente from './components/PrimerComponente';
-import ComponenteUsuario from './components/ComponenteUsuario';
-import ComponenteGestorUsuarios from './components/ComponenteGestorUsuarios';
-import BotonPDF from './components/BotonPDF';
+import DashboardLogin from './components/DashboardLogin';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PrimerComponente />} />
-        <Route path="/usuarios" element={<ComponenteUsuario />} />
-        <Route path="/GestionarUsuarios" element={<ComponenteGestorUsuarios />} />
-        <Route path="/Pdf" element={<BotonPDF />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <DashboardLogin />
+    </div>
   );
 }
 
