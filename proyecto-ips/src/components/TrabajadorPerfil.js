bcc00b8db3fc5021f665ab6898c6845316e52685
Gersson Huichi <ghuichi@unsa.edu.pe>
2024-07-18 18:22:16 -0500

    Correción en los diseños de Perfil y Turno

--- proyecto-ips/src/components/TrabajadorPerfil.js
+++ proyecto-ips/src/components/TrabajadorPerfil.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TrabajadorPerfil.css';
 
-const WorkerProfile = () => {
+const TrabajadorPerfil = () => {
     // Datos del trabajador (simulación)
     const trabajador = {
         nombre: 'Juan Pérez',
@@ -18,28 +18,40 @@ const WorkerProfile = () => {
     };
 
     return (
-        <div className="profile-container">
-            <div className="profile-content">
-                <div className="profile-about">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5jQBCUrSznF87uovPNohqPnO6Kr-maYEge54VLk0_BKg2UlxW1_lG4FfZkZ7eLKn13Ko&usqp=CAU" alt="Trabajador" width='300px' height='400px'/>
-                    <p><strong>Empresa:</strong> {trabajador.empresa}</p>
-                    <p><strong>Cargo:</strong> {trabajador.cargo}</p>
-                    <p><strong>ID Trabajador:</strong> {trabajador.idTrabajador}</p>
-                    <p><strong>Categoría Laboral:</strong> {trabajador.categoriaLaboral}</p>
-                </div>
-                <div className="profile-details">
-                    <h2>Datos Personales</h2>
-                    <p><strong>Nombre:</strong> {trabajador.nombre}</p>
-                    <p><strong>Edad:</strong> {trabajador.edad} años</p>
-                    <p><strong>Ubicación:</strong> {trabajador.ubicacion}</p>
-                    <p><strong>DNI:</strong> {trabajador.dni}</p>
-                    <p><strong>Vivienda:</strong> {trabajador.vivienda}</p>
-                    <p><strong>Estado Civil:</strong> {trabajador.estadoCivil}</p>
-                    <p><strong>Tipo de Sandre:</strong> {trabajador.tipoSangre}</p>
+        <div className="fondo">
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="#perfil" className="opciones">Perfil</a></li>
+                        <li><a href="#turno" className="opciones">Turno</a></li>
+                        <li><a href="#ruta" className="opciones">Ruta</a></li>
+                        <li><a href="#login" className="crear">Login</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <div className="profile-container">
+                <div className="profile-content">
+                    <div className="profile-about">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5jQBCUrSznF87uovPNohqPnO6Kr-maYEge54VLk0_BKg2UlxW1_lG4FfZkZ7eLKn13Ko&usqp=CAU" alt="Trabajador" className="profile-image" />
+                        <p><strong>Empresa:</strong> {trabajador.empresa}</p>
+                        <p><strong>Cargo:</strong> {trabajador.cargo}</p>
+                        <p><strong>ID Trabajador:</strong> {trabajador.idTrabajador}</p>
+                        <p><strong>Categoría Laboral:</strong> {trabajador.categoriaLaboral}</p>
+                    </div>
+                    <div className="profile-details">
+                        <h2>Datos Personales</h2>
+                        <p><strong>Nombre:</strong> {trabajador.nombre}</p>
+                        <p><strong>Edad:</strong> {trabajador.edad} años</p>
+                        <p><strong>Ubicación:</strong> {trabajador.ubicacion}</p>
+                        <p><strong>DNI:</strong> {trabajador.dni}</p>
+                        <p><strong>Vivienda:</strong> {trabajador.vivienda}</p>
+                        <p><strong>Estado Civil:</strong> {trabajador.estadoCivil}</p>
+                        <p><strong>Tipo de Sangre:</strong> {trabajador.tipoSangre}</p>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
 
-export default WorkerProfile;
+export default TrabajadorPerfil;
--- proyecto-ips/src/components/TrabajadorTurno.js
+++ proyecto-ips/src/components/TrabajadorTurno.js
@@ -1,39 +1,45 @@
 import React from 'react';
 import './TrabajadorTurno.css';
 
-const WorkerView = () => {
+const TrabajadorTurno = () => {
     return (
-        <div className="worker-view">
-            <header className="header">
-                <h1>Turno 1A</h1>
-                <p>Domingo 17 Mayo 07:00 am - 8:30 am</p>
+        <div className="fondo">
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="#perfil" className="opciones">Perfil</a></li>
+                        <li><a href="#turno" className="opciones">Turno</a></li>
+                        <li><a href="#ruta" className="opciones">Ruta</a></li>
+                        <li><a href="#login" className="crear">Login</a></li>
+                    </ul>
+                </nav>
             </header>
-            <section className="info-section">
-                <div className="info-card">
-                    <h2>Información</h2>
-                    <p><strong>Destino:</strong> Polobaya - Arequipa</p>
-                    <p><strong>D. Turno:</strong> 1A</p>
-                    <p><strong>Fecha:</strong> 17 de mayo del 2023</p>
-                    <p><strong>Bus:</strong> ABC-123</p>
-                </div>
-                <div className="driver-card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7I7HyE4katbJHadjFl6NyQF9oFqqnBHbGGA&s" alt="Conductor" width='300px' height='400px' />
-                    <h2>José Manuel Jaita Chura</h2>
-                    <p><strong>ID:</strong> 088213213</p>
-                </div>
-            </section>
-            <section className="bus-info">
-                <p><strong>Placa de Bus:</strong> ABC-123</p>
-                <p><strong>ID Bus:</strong> Tr09287</p>
-            </section>
-            <nav className="bottom-nav">
-                <button>Perfil</button>
-                <button>Turno</button>
-                <button>Ruta</button>
-                <button>Login</button>
-            </nav>
+            <div className="worker-view">
+                <header className="header">
+                    <h1>Turno 1A</h1>
+                    <p>Domingo 17 Mayo 07:00 am - 8:30 am</p>
+                </header>
+                <section className="info-section">
+                    <div className="info-card">
+                        <h2>Información</h2>
+                        <p><strong>Destino:</strong> Polobaya - Arequipa</p>
+                        <p><strong>D. Turno:</strong> 1A</p>
+                        <p><strong>Fecha:</strong> 17 de mayo del 2023</p>
+                        <p><strong>Bus:</strong> ABC-123</p>
+                    </div>
+                    <div className="driver-card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7I7HyE4katbJHadjFl6NyQF9oFqqnBHbGGA&s" alt="Conductor" className="driver-image" />
+                        <h2>José Manuel Jaita Chura</h2>
+                        <p><strong>ID:</strong> 088213213</p>
+                    </div>
+                </section>
+                <section className="bus-info">
+                    <p><strong>Placa de Bus:</strong> ABC-123</p>
+                    <p><strong>ID Bus:</strong> Tr09287</p>
+                </section>
+            </div>
         </div>
     );
 }
 
-export default WorkerView;
+export default TrabajadorTurno;
