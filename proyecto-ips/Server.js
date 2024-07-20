d1719d05d87f8cbf0827e31a4a3c29fed34ca4b7
AndreRH09 <aanazcoh@unsa.edu.pe>
2024-07-20 17:29:44 -0500

    cambios en ditar usuario e insertar

--- proyecto-ips/Server.js
+++ proyecto-ips/Server.js
@@ -1,7 +1,9 @@
-//D:\Nueva carpeta\Sistema_Control_Buses\proyecto-ips\Server.js
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const port = 3001;
@@ -15,6 +17,10 @@ app.use(
 
 // Middleware para analizar JSON
 app.use(express.json());
+
+// Configuración de Multer para manejar la carga de archivos
+const storage = multer.memoryStorage(); // Guarda archivos en memoria
+const upload = multer({ storage: storage });
 
 // Configuración de la conexión a la base de datos
 const db = mysql.createConnection({
@@ -32,12 +38,37 @@ db.connect((err) => {
   console.log("Conectado a la Base de Datos");
 });
 
+// Función para manejar errores
+const handleError = (res, err, message) => {
+  console.error(message, err);
+  res.status(500).send(message);
+};
+
+// Función de validación
+const validateUser = async (user) => {
+  const { Edad, img } = user;
+
+  // Validar el rango de edad
+  const edad = parseInt(Edad);
+  if (edad < 18 || edad > 60) {
+    throw new Error("La edad debe estar entre 18 y 60 años");
+  }
+
+  // Validar el archivo de imagen
+  if (img) {
+    const { default: fileType } = await import("file-type");
+    const type = await fileType.fromBuffer(img);
+    if (!type || !type.mime.startsWith("image/")) {
+      throw new Error("El archivo subido no es una imagen válida");
+    }
+  }
+};
+
 // Ruta para obtener todos los usuarios
 app.get("/api/data", (req, res) => {
   db.query("SELECT * FROM Usuario", (err, results) => {
     if (err) {
-      res.status(500).send(err);
-      return;
+      return handleError(res, err, "Error al obtener usuarios");
     }
     res.json(results);
   });
@@ -49,8 +80,11 @@ app.get("/api/maxCodigoUsuario", (req, res) => {
     "SELECT MAX(Codigo_Usuario) AS maxCodigoUsuario FROM Usuario",
     (err, results) => {
       if (err) {
-        res.status(500).send(err);
-        return;
+        return handleError(
+          res,
+          err,
+          "Error al obtener el código máximo de usuario"
+        );
       }
       const maxCodigoUsuario = results[0].maxCodigoUsuario || 0; // Si no hay usuarios, devuelve 0
       res.json({ maxCodigoUsuario });
@@ -58,18 +92,55 @@ app.get("/api/maxCodigoUsuario", (req, res) => {
   );
 });
 
-app.post("/api/insert", (req, res) => {
-  const nuevoUsuario = req.body; // Datos del usuario a insertar
+// Ruta para insertar un nuevo usuario
+app.post("/api/insert", upload.single("img"), async (req, res) => {
+  const nuevoUsuario = req.body;
+  try {
+    await validateUser(nuevoUsuario);
 
-  db.query("INSERT INTO Usuario SET ?", nuevoUsuario, (err, result) => {
-    if (err) {
-      console.error("Error al insertar usuario:", err);
-      res.status(500).send("Error al insertar usuario");
-      return;
-    }
-    console.log("Usuario insertado correctamente");
-    res.send("Usuario insertado exitosamente");
-  });
+    const query = `INSERT INTO Usuario (
+      Codigo_Usuario,
+      Nombre,
+      Nombre_Usuario,
+      Contrasena,
+      DNI,
+      Codigo_Cargo,
+      Edad,
+      Sexo,
+      Celular,
+      Email,
+      Direccion,
+      EstadoRegistro,
+      img
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    db.query(
+      query,
+      [
+        nuevoUsuario.Codigo_Usuario,
+        nuevoUsuario.Nombre,
+        nuevoUsuario.Nombre_Usuario,
+        nuevoUsuario.Contrasena,
+        nuevoUsuario.DNI,
+        nuevoUsuario.Codigo_Cargo,
+        nuevoUsuario.Edad,
+        nuevoUsuario.Sexo,
+        nuevoUsuario.Celular,
+        nuevoUsuario.Email,
+        nuevoUsuario.Direccion,
+        nuevoUsuario.EstadoRegistro,
+        req.file ? req.file.buffer : null,
+      ],
+      (err, result) => {
+        if (err) {
+          return handleError(res, err, "Error al insertar usuario");
+        }
+        res.send("Usuario insertado exitosamente");
+      }
+    );
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 });
 
 // Ruta para obtener un usuario por su código
@@ -80,12 +151,10 @@ app.get("/api/data/:codigoUsuario", (req, res) => {
     [codigoUsuario],
     (err, results) => {
       if (err) {
-        res.status(500).send(err);
-        return;
+        return handleError(res, err, "Error al obtener usuario");
       }
       if (results.length === 0) {
-        res.status(404).send("Usuario no encontrado");
-        return;
+        return res.status(404).send("Usuario no encontrado");
       }
       res.json(results[0]);
     }
@@ -93,23 +162,79 @@ app.get("/api/data/:codigoUsuario", (req, res) => {
 });
 
 // Ruta para actualizar un usuario por su código
-app.put("/api/update/:codigoUsuario", (req, res) => {
-  const codigoUsuario = req.params.codigoUsuario;
-  const updatedUser = req.body;
+app.put(
+  "/api/update/:codigoUsuario",
+  upload.single("img"),
+  async (req, res) => {
+    const codigoUsuario = req.params.codigoUsuario;
+    const updatedUser = req.body;
+    const img = req.file ? req.file.buffer : null;
 
-  db.query(
-    "UPDATE Usuario SET ? WHERE Codigo_Usuario = ?",
-    [updatedUser, codigoUsuario],
-    (err, result) => {
-      if (err) {
-        res.status(500).send(err);
-        return;
-      }
-      res.send("Usuario actualizado exitosamente");
+    try {
+      // Validar los datos del usuario
+      await validateUser(updatedUser);
+
+      // Consultar usuario actual para obtener la imagen si no se proporciona una nueva
+      const [currentUser] = await new Promise((resolve, reject) => {
+        db.query(
+          "SELECT img FROM Usuario WHERE Codigo_Usuario = ?",
+          [codigoUsuario],
+          (err, results) => {
+            if (err) return reject(err);
+            resolve(results);
+          }
+        );
+      });
+
+      const existingImg = currentUser.img;
+
+      // Actualizar el usuario
+      const query = `UPDATE Usuario SET 
+      Nombre = ?, 
+      Nombre_Usuario = ?, 
+      Contrasena = ?, 
+      DNI = ?, 
+      Codigo_Cargo = ?, 
+      Edad = ?, 
+      Sexo = ?, 
+      Celular = ?, 
+      Email = ?, 
+      Direccion = ?, 
+      EstadoRegistro = ?, 
+      img = ? 
+      WHERE Codigo_Usuario = ?`;
+
+      db.query(
+        query,
+        [
+          updatedUser.Nombre,
+          updatedUser.Nombre_Usuario,
+          updatedUser.Contrasena,
+          updatedUser.DNI,
+          updatedUser.Codigo_Cargo,
+          updatedUser.Edad,
+          updatedUser.Sexo,
+          updatedUser.Celular,
+          updatedUser.Email,
+          updatedUser.Direccion,
+          updatedUser.EstadoRegistro,
+          img || existingImg, // Si no hay una imagen nueva, conserva la imagen existente
+          codigoUsuario,
+        ],
+        (err, result) => {
+          if (err) {
+            return handleError(res, err, "Error al actualizar usuario");
+          }
+          res.send("Usuario actualizado exitosamente");
+        }
+      );
+    } catch (err) {
+      res.status(400).send(err.message);
     }
-  );
-});
+  }
+);
 
+// Ruta para eliminar un usuario por su código
 app.delete("/api/delete/:codigoUsuario", (req, res) => {
   const codigoUsuario = req.params.codigoUsuario;
 
@@ -118,23 +243,21 @@ app.delete("/api/delete/:codigoUsuario", (req, res) => {
     [codigoUsuario],
     (err, result) => {
       if (err) {
-        res.status(500).send(err);
-        return;
+        return handleError(res, err, "Error al eliminar usuario");
       }
       if (result.affectedRows === 0) {
-        res.status(404).send("Usuario no encontrado");
-        return;
+        return res.status(404).send("Usuario no encontrado");
       }
       res.send("Usuario eliminado exitosamente");
     }
   );
 });
 
+// Ruta para obtener todos los cargos
 app.get("/api/cargos", (req, res) => {
   db.query("SELECT Codigo_Cargo, Descripcion FROM Cargo", (err, results) => {
     if (err) {
-      res.status(500).send(err);
-      return;
+      return handleError(res, err, "Error al obtener cargos");
     }
     res.json(results);
   });
