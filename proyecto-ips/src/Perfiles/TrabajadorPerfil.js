867954f657b6e9e446769a0d86d90f81afa17347
Gersson Huichi <ghuichi@unsa.edu.pe>
2024-07-22 14:56:33 -0500

    Correción JS TrabajadorPerfil parte02, gracias por nada Willy

--- proyecto-ips/src/Perfiles/TrabajadorPerfil.js
+++ proyecto-ips/src/Perfiles/TrabajadorPerfil.js
@@ -16,3 +16,22 @@ const TrabajadorPerfil = () => {
         tipoSangre: 'O+',
         categoriaLaboral: 'Contratista',
     };
+
+    return (
+        <div className={style.fondo}>
+            <header className={style.header}>
+                <nav className={style.nav}>
+                    <ul className={style.ul}>
+                        <li className={style.li}><a href="#perfil" className={style.aopciones}>Perfil</a></li>
+                        <li className={style.li}><a href="#turno" className={style.aopciones}>Turno</a></li>
+                        <li className={style.li}><a href="#ruta" className={style.aopciones}>Ruta</a></li>
+                        <li className={style.li}><a href="#login" className={style.acrear}>Login</a></li>
+                    </ul>
+                </nav>
+            </header>
+            
+        </div>
+    );
+}
+
+export default TrabajadorPerfil;
