885948f5c5fcb100d29a2ff1b6c390dda2f6d996
Willy Alexander Huisa Perez <whuisa@unsa.edu.pe>
2024-07-22 01:17:32 -0500

    Se elimino el import al css general

--- proyecto-ips/src/Turno/TrabajadorTurno.js
+++ proyecto-ips/src/Turno/TrabajadorTurno.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './TrabajadorTurno.css';
 
 const TrabajadorTurno = () => {
     return (
--- proyecto-ips/src/Vistas/GestionarBuses.js
+++ proyecto-ips/src/Vistas/GestionarBuses.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import "./GestionarBuses.css";
 
 const GestionarBuses = ({ className = "" }) => {
   return (   
