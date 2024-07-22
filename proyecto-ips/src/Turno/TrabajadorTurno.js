c69a13c0d726f66d04aaeb9fdc1f8505dcaf78f7
Gersson Huichi <ghuichi@unsa.edu.pe>
2024-07-22 14:43:43 -0500

    Correción de nuevo de las vistas del Trabajador, gracias por nada Willy

--- proyecto-ips/src/Turno/TrabajadorTurno.js
+++ proyecto-ips/src/Turno/TrabajadorTurno.js
@@ -1,38 +1,40 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import style from './TrabajadorTurno.module.css';
 
 const TrabajadorTurno = () => {
     return (
-        <div className="fondo">
-            <header>
-                <nav>
-                    <ul>
-                        <li><a href="#perfil" className="opciones">Perfil</a></li>
-                        <li><a href="#turno" className="opciones">Turno</a></li>
-                        <li><a href="#ruta" className="opciones">Ruta</a></li>
-                        <li><a href="#login" className="crear">Login</a></li>
+        <div className={style.fondo}>
+            <header className={style.header}>
+                <nav className={style.nav}>
+                    <ul className={style.ul}>
+                        <li className={style.li}><a href="#perfil" className={style.aopciones}>Perfil</a></li>
+                        <li className={style.li}><a href="#turno" className={style.aopciones}>Turno</a></li>
+                        <li className={style.li}><a href="#ruta" className={style.aopciones}>Ruta</a></li>
+                        <li className={style.li}><a href="#login" className={style.acrear}>Login</a></li>
                     </ul>
                 </nav>
             </header>
-            <div className="worker-view">
-                <header className="header">
+            <div className={style.workerView}>
+                <header className={style.turnoHeader}>
                     <h1>Turno 1A</h1>
                     <p>Domingo 17 Mayo 07:00 am - 8:30 am</p>
                 </header>
-                <section className="info-section">
-                    <div className="info-card">
+                <section className={style.infoSection}>
+                    <div className={style.infoCard}>
                         <h2>Información</h2>
                         <p><strong>Destino:</strong> Polobaya - Arequipa</p>
                         <p><strong>D. Turno:</strong> 1A</p>
                         <p><strong>Fecha:</strong> 17 de mayo del 2023</p>
                         <p><strong>Bus:</strong> ABC-123</p>
                     </div>
-                    <div className="driver-card">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7I7HyE4katbJHadjFl6NyQF9oFqqnBHbGGA&s" alt="Conductor" className="driver-image" />
+                    <div className={style.driverCard}>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7I7HyE4katbJHadjFl6NyQF9oFqqnBHbGGA&s" alt="Conductor" className={style.driverImage} />
                         <h2>José Manuel Jaita Chura</h2>
                         <p><strong>ID:</strong> 088213213</p>
                     </div>
                 </section>
-                <section className="bus-info">
+                <section className={style.busInfo}>
                     <p><strong>Placa de Bus:</strong> ABC-123</p>
                     <p><strong>ID Bus:</strong> Tr09287</p>
                 </section>
@@ -40,5 +42,9 @@ const TrabajadorTurno = () => {
         </div>
     );
 }
+
+TrabajadorTurno.propTypes = {
+    className: PropTypes.string,
+};
 
 export default TrabajadorTurno;
