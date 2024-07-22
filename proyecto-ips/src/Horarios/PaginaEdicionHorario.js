17b7d3e3eb23bb7f20304335a0093dc054fa7a30
Willy Alexander Huisa Perez <whuisa@unsa.edu.pe>
2024-07-22 00:11:47 -0500

    Se edito y se le aplico css a la edicion de horario

--- proyecto-ips/src/Horarios/PaginaEdicionHorario.js
+++ proyecto-ips/src/Horarios/PaginaEdicionHorario.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import style from './PaginaEdicionHorario.module.css';
 
 const PaginaEdicionHorario = () => {
   const { idHorario } = useParams();
@@ -72,12 +73,25 @@ const PaginaEdicionHorario = () => {
   }
 
   return (
-    <div>
-      <h2>Editar Horario</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
+    <div className={style.fondo}>
+      <header className={style.header}>
+        <nav className={style.nav}>
+          <ul className={style.ul}>
+          <li className={style.li}><a className={style.aopciones} href="#" >Horario</a></li>
+          <li className={style.li}><a className={style.aopciones} href="#" >Bus</a></li>
+          <li className={style.li}><a className={style.aopciones} href="#" >Ruta</a></li>
+          <li className={style.li}><a className={style.acrear} href="#" >Crear Turno</a></li>
+          <li className={style.li}><a className={style.acrear} href="#" >Crear Usuario</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div className={style.EdicionHorario}>
+      <form className={style.formEdicionRuta} onSubmit={handleSubmit}>
+      <h2 className={style.titulo}>Editar Horario</h2>
+      <div className={style.campos}>
+        <label className={style.label1}>
           Fecha:
-          <input
+          <input className={style.input}
             type="date"
             name="Fecha"
             value={horario.Fecha}
@@ -85,9 +99,9 @@ const PaginaEdicionHorario = () => {
             required // Campo obligatorio
           />
         </label>
-        <label>
+        <label className={style.label1}>
           Hora de Salida:
-          <input
+          <input className={style.input}
             type="time"
             name="Hora_Salida"
             value={horario.Hora_Salida}
@@ -95,9 +109,9 @@ const PaginaEdicionHorario = () => {
             required // Campo obligatorio
           />
         </label>
-        <label>
+        <label className={style.label1}>
           Hora de Llegada:
-          <input
+          <input className={style.input}
             type="time"
             name="Hora_Llegada"
             value={horario.Hora_Llegada}
@@ -105,8 +119,13 @@ const PaginaEdicionHorario = () => {
             required // Campo obligatorio
           />
         </label>
-        <button type="submit">Actualizar</button>
+        </div>
+        <div className={style.botones}>
+        <button className={style.boton1} type="submit">Actualizar</button>
+        <button className={style.boton2} type="submit">Cancelar</button>
+        </div>
       </form>
+      </div>
     </div>
   );
 };
