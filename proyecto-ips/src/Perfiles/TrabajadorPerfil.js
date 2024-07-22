032bc0a67f73ed144ced42ca1b9dfd0523b0c200
Gersson Huichi <ghuichi@unsa.edu.pe>
2024-07-22 14:57:04 -0500

    Correción JS TrabajadorPerfil parte03, gracias por nada Willy

--- proyecto-ips/src/Perfiles/TrabajadorPerfil.js
+++ proyecto-ips/src/Perfiles/TrabajadorPerfil.js
@@ -29,7 +29,27 @@ const TrabajadorPerfil = () => {
                     </ul>
                 </nav>
             </header>
-            
+            <div className={style.profileContainer}>
+                <div className={style.profileContent}>
+                    <div className={style.profileAbout}>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5jQBCUrSznF87uovPNohqPnO6Kr-maYEge54VLk0_BKg2UlxW1_lG4FfZkZ7eLKn13Ko&usqp=CAU" alt="Trabajador" className={style.profileImage} />
+                        <p><strong>Empresa:</strong> {trabajador.empresa}</p>
+                        <p><strong>Cargo:</strong> {trabajador.cargo}</p>
+                        <p><strong>ID Trabajador:</strong> {trabajador.idTrabajador}</p>
+                        <p><strong>Categoría Laboral:</strong> {trabajador.categoriaLaboral}</p>
+                    </div>
+                    <div className={style.profileDetails}>
+                        <h2>Datos Personales</h2>
+                        <p><strong>Nombre:</strong> {trabajador.nombre}</p>
+                        <p><strong>Edad:</strong> {trabajador.edad} años</p>
+                        <p><strong>Ubicación:</strong> {trabajador.ubicacion}</p>
+                        <p><strong>DNI:</strong> {trabajador.dni}</p>
+                        <p><strong>Vivienda:</strong> {trabajador.vivienda}</p>
+                        <p><strong>Estado Civil:</strong> {trabajador.estadoCivil}</p>
+                        <p><strong>Tipo de Sangre:</strong> {trabajador.tipoSangre}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
